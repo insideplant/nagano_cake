176e5f00e5cca9607deaa64a127789c5fecd5d21
EC2 Default User <ec2-user@ip-172-31-1-112.ec2.internal>
2021-04-27 13:42:32 +0000

    add gem'devise',g controller homes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :rails
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :orders
+  
 end
