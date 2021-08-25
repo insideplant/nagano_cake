855394dc00699210565505d9279e4e3587b61528
insideplant <kami.10pearl@gmail.com>
2021-08-25 15:02:27 +0000

    test6

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -4,3 +4,5 @@
 #   http_host: 'example.org',
 #   https: false
 # )
+
+Refile.secret_key = 'ca74fadf2d04a9bcafc42ae494a104e71313dc0a8f1e2065e418a7f58880ec967a4f2083f9db6f15838e9bcec6875b03a82fcb0ccba3fb19de050793f1874f3d'
