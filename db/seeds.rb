51f1b79ab43d2e29619cc3bdb5378cbb3b947687
insideplant <kami.10pearl@gmail.com>
2021-08-31 13:11:24 +0000

    rails seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  # genres
+  Genre.create!(
+    [
+      {
+        genre_name: "スポンジケーキ"
+      },
+      {
+        genre_name: "誕生日ケーキ"
+      },
+      {
+        genre_name: "アイニバーサリーケーキ"
+      },
+    ]
+  )
+
+  Item.create!(
+    [
+      {
+        genre_id: "1",
+        name: "ショートケーキ",
+        image: File.open("./app/assets/images/cake1.jpg"),
+        introduction: "ショートケーキです。",
+        price: "800",
+        is_active: true,
+      },
+      {
+        genre_id: "2",
+        name: "ブルーベリーケーキ",
+        image: File.open("./app/assets/images/cake2.jpg"),
+        introduction: "ブルーベリーケーです。",
+        price: "600",
+        is_active: true,
+      },
+      {
+        genre_id: "3",
+        name: "キュウリケーキ",
+        image: File.open("./app/assets/images/cake3.jpg"),
+        introduction: "キュウリケーキです。",
+        price: "1000",
+        is_active: true,
+      },
+      {
+        genre_id: "2",
+        name: "ブロッコリーケーキ",
+        image: File.open("./app/assets/images/cake4.jpg"),
+        introduction: "ブロッコリーケーキです。",
+        price: "800",
+        is_active: true,
+      }
+    ]
+  )
